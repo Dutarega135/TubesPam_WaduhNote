ae38986da440d869504f0a674be05389a6a304ee
Bagusardin <94891965+Bagusardin@users.noreply.github.com>
2023-05-20 22:44:23 +0700

    Add files via upload

---
+++ config2/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+    light : {
+        theme:"light",
+        color:"black",
+        background:"#5755CF",
+    },dark : {
+        theme:"dark",
+        color:"white",
+        background:"black",
+    },
+};
+export default theme;
